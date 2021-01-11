c5e04123b99ec0507bab02a8a7cd51875f09c1e6
Skias <loiccena@live.be>
2021-01-11 14:52:03 +0100

    Auto stash before merge of "main" and "origin/main"

--- GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/dao/LearningActivityDAO.java
+++ GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/dao/LearningActivityDAO.java
@@ -3,8 +3,10 @@ package be.helha.groupeB4.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 import be.helha.groupeB4.entities.LearningActivity;
+import be.helha.groupeB4.entities.LearningUnit;
 
 @Stateless
 public class LearningActivityDAO extends AbstractDAO implements ILearningActivityDAO{
@@ -12,19 +14,25 @@ public class LearningActivityDAO extends AbstractDAO implements ILearningActivit
 	@Override
 	public LearningActivity addLearningActivity(LearningActivity aa) {
 		// TODO Auto-generated method stub
-		return null;
+		em.persist(aa);
+		return aa;
 	}
 
 	@Override
 	public LearningActivity getLearningActivity(String identification) {
 		// TODO Auto-generated method stub
-		return null;
+		String sqlRequestStr = "SELECT learningActivity FROM LearningActivity learningActivity "
+                + "WHERE learningActivity.id = ?1 ";
+		TypedQuery<LearningActivity> query = em.createQuery(sqlRequestStr,LearningActivity.class);
+		query.setParameter(1, identification);
+		List<LearningActivity> result = query.getResultList();
+		return result.isEmpty()? null:result.get(0);
 	}
 
 	@Override
 	public List<LearningActivity> getAllLearningActivities() {
 		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("SELECT learningActivity FROM LearningActivity learningActivity").getResultList();
 	}
 
 }
--- GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/dao/LearningUnitDAO.java
+++ GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/dao/LearningUnitDAO.java
@@ -3,6 +3,7 @@ package be.helha.groupeB4.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 import be.helha.groupeB4.entities.LearningActivity;
 import be.helha.groupeB4.entities.LearningUnit;
@@ -13,25 +14,32 @@ public class LearningUnitDAO extends AbstractDAO implements ILearningUnitDAO{
 	@Override
 	public LearningUnit addLearningUnit(LearningUnit ue) {
 		// TODO Auto-generated method stub
-		return null;
+		em.persist(ue);
+		return ue;
 	}
 
 	@Override
 	public LearningUnit getLearningUnit(String identification) {
 		// TODO Auto-generated method stub
-		return null;
+		String sqlRequestStr = "SELECT learningUnit from LearningUnit learningUnit "
+                + "WHERE learningUnit.id = ?1 ";
+		TypedQuery<LearningUnit> query = em.createQuery(sqlRequestStr,LearningUnit.class);
+		query.setParameter(1, identification);
+		List<LearningUnit> result = query.getResultList();
+		return result.isEmpty()? null:result.get(0);
 	}
 
 	@Override
 	public List<LearningUnit> getAllLearningUnits() {
 		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("SELECT learningUnit FROM LearningUnit learningUnit").getResultList();
 	}
 
 	@Override
 	public List<LearningActivity> getAllLearningActivities() {
 		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("SELECT learningUnit FROM LearningUnit learningUnit "
+				+ "JOIN learningUnit.aaList learningActivity").getResultList();
 	}
 
 }
