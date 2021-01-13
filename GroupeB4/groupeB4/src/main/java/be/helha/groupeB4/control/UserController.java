c5a870c98912780ec3f758a027ef6a1c37f5d2de
Frozzie <froztor85@gmail.com>
2021-01-13 15:38:46 +0100

    progress

--- GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/control/UserController.java
+++ GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/control/UserController.java
@@ -1,6 +1,10 @@
 package be.helha.groupeB4.control;
 
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
@@ -13,29 +17,40 @@ import be.helha.groupeB4.entities.User;
 @Named
 @SessionScoped
 public class UserController implements Serializable{
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Inject
 	private UserEJB ejb;
 	
+	private User user;
+	private String passwordConfirm;
+	
 	public List<User> doSelectAll(){
 		return ejb.getAllUsers(); 
 	}
 	
-	
 	public User addUser() {
-		User u = new User();
-		ejb.addUser(u);
-		return u;
+		MessageDigest digest = null;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    byte[] byteOfTextToHash = passwordConfirm.getBytes(StandardCharsets.UTF_8);
+	    byte[] hashedByetArray = digest.digest(byteOfTextToHash);
+	
+	    String encodedPasswordConfirm = Base64.getEncoder().encodeToString(hashedByetArray);
+		
+	    
+	    if (encodedPasswordConfirm.equals(user.getPassword())) {	    	
+			ejb.addUser(user);
+	    }
+	    
+	    return user;
 	}
-	public void deleteUser(String userName){
-		User u = ejb.getUser(userName);
-		ejb.deleteUser(u);
-		return;
+	public void deleteUser(){
+		ejb.deleteUser(user);
 	}
 	
 	
