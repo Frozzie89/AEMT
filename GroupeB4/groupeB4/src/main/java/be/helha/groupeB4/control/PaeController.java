baa9146195d73c6dae50fe37d2e3f0b7a33b3a92
hugo <hugolevecq3@gmail.com>
2021-01-14 16:43:32 +0100

    Creation.xhtml works at the moment

--- GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/control/PaeController.java
+++ GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/control/PaeController.java
@@ -10,9 +10,12 @@ import javax.inject.Named;
 
 import be.helha.groupeB4.ejb.PaeEJB;
 import be.helha.groupeB4.ejb.StudentEJB;
+import be.helha.groupeB4.entities.LearningActivity;
 import be.helha.groupeB4.entities.LearningUnit;
 import be.helha.groupeB4.entities.Pae;
 import be.helha.groupeB4.entities.Student;
+import be.helha.groupeB4.enumeration.EPaeProgress;
+import be.helha.groupeB4.enumeration.ESection;
 
 @ManagedBean
 @Named("pc")
@@ -44,7 +47,32 @@ public class PaeController implements Serializable{
 	
 	
 	public Pae doSelectPae(){
-		return ejb.getPAE(student.getPae()); 
+		
+		Student s1 = new Student("s1", "s1", "la1", "y1", 1, ESection.INFORMATIQUE_DE_GESTION);
+		System.out.println("s1: " + s1.toString());
+		
+		Pae p1 = new Pae(EPaeProgress.TERMINE);
+		
+		LearningUnit ue1 = new LearningUnit("UE1", "ue1Label", 10, "y1");
+		LearningUnit ue2 = new LearningUnit("UE2", "ue2Label", 10, "y2");
+		
+		LearningActivity aa1 = new LearningActivity("AA1", "aa1Label", 5);		
+		LearningActivity aa2 = new LearningActivity("AA2", "aa2Label", 5);	
+		LearningActivity aa3 = new LearningActivity("AA3", "aa3Label", 5);	
+		
+		ue1.addLearning(aa1);
+		ue1.addLearning(aa2);
+		ue2.addLearning(aa3);
+		
+		p1.addLearningUnit(ue1);
+		p1.addLearningUnit(ue2);
+		
+		s1.setPae(p1);
+		
+		
+		
+		return s1.getPae();
+		//return ejb.getPAE(student.getPae()); 
 	}
 	
 	public List<LearningUnit> getlists(){
