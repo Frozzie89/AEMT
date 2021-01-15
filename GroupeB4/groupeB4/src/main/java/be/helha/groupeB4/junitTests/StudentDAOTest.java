512050130c8e6e66ceb6985a8d77bee216e49089
Frozzie <froztor85@gmail.com>
2021-01-15 16:35:10 +0100

    started base of Junit tests

---
+++ GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/junitTests/StudentDAOTest.java
@@ -0,0 +1,65 @@
+package be.helha.groupeB4.junitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import be.helha.groupeB4.dao.StudentDAO;
+
+public class StudentDAOTest {
+	
+	private StudentDAO studentDao;
+
+	@Before
+	public void setUp() throws Exception {
+		studentDao = new StudentDAO();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetListe() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddStudent() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddStudents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllStudent() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDeleteStudent() {
+		fail("Not yet implemented");
+	}
+	
+	@Test 
+	void testUpdateStudentr() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetAllStudentByName() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetStudentByID() {
+		fail("Not yet implemented");
+	}
+}
