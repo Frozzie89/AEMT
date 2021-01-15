0928f53dad83cdb07a2e7ed4a0c745bd7fc78358
De Bels <lievendebels.isf@gmail.com>
2021-01-15 20:43:47 +0100

    PAE create : Add fonctionne. Remove crash.

--- GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/control/PaeController.java
+++ GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/control/PaeController.java
@@ -1,6 +1,7 @@
 package be.helha.groupeB4.control;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
@@ -8,6 +9,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.jboss.weld.module.EjbSupport;
 
 import be.helha.groupeB4.ejb.PaeEJB;
 import be.helha.groupeB4.ejb.StudentEJB;
@@ -31,7 +34,13 @@ public class PaeController implements Serializable{
 	@Inject
 	private PaeEJB ejb;
 
+	@Inject
+	private StudentEJB ejbStudent;
+
 	private Student student ;
+	private Student studentCopy ;
+	
+	private List<LearningUnit> listUEAvailable;
 
 	public Student getStudent() {
 		return student;
@@ -40,11 +49,28 @@ public class PaeController implements Serializable{
 	public void setStudent(Student student) {
 		this.student = student;
 	}
-	
+
+	public Student getStudentCopy() {
+		return studentCopy;
+	}
+
+	public void setStudentCopy(Student studentCopy) {
+		this.studentCopy = studentCopy;
+	}
+
+	public List<LearningUnit> getListUEAvailable() {
+		return listUEAvailable;
+	}
+
+	public void setListUEAvailable(List<LearningUnit> listUEAvailable) {
+		this.listUEAvailable = listUEAvailable;
+	}
+
 	public String doSelectPae(Student s){
 		
 		setStudent(s);
-		student = s;
+		setStudentCopy(s);
+		student.getPae().setPaeProgress(EPaeProgress.EN_COURS);
 		return "paeCreation.xhtml?faces-redirect-true";
 	}
 	
@@ -61,13 +87,29 @@ public class PaeController implements Serializable{
 	
 	public void addUEToPAE(LearningUnit lu){
 		student.getPae().addLearningUnit(lu);
+		updatePae();
 	}
 	
 	public void removeUEFromPAE(LearningUnit lu){
 		student.getPae().removeLearningUnit(lu);
+		updatePae();
 	}
 	
 	
+	public void getAllUEFromSection(){
+		LearningUnitController luc = new LearningUnitController();
+		List<LearningUnit> tmp = new ArrayList<>();
+		tmp = student.getPae().getUeList();
+		listUEAvailable =  luc.doSelectSectionUE(student);
+		for (int i = 0; i < tmp.size(); i++) {
+			if(listUEAvailable.contains(tmp.get(i))) {
+				listUEAvailable.remove(tmp.get(i));
+			}
+		}
+	}
 	
+	public void updatePae() {
+		ejb.updatePae(studentCopy.getPae(), student.getPae());
+	}
 
 }
