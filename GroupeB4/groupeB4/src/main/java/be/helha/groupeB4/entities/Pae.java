eb9bd028ee06e8a29da7b88d475b3338ac37068d
Skias <loiccena@live.be>
2021-01-17 11:00:29 +0100

    Change NamePage
    Change name page and css style

--- GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/entities/Pae.java
+++ GroupeB4/groupeB4/src/main/java/be/helha/groupeB4/entities/Pae.java
@@ -22,8 +22,7 @@ public class Pae implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-//	@OneToMany(fetch = FetchType.EAGER, orphanRemoval= true,cascade= {CascadeType.ALL})
-	@CascadeOnDelete
+	@OneToMany(fetch = FetchType.EAGER, cascade= {CascadeType.PERSIST})
 	private List<LearningUnit> ueList;
 	
 	private EPaeProgress paeProgress;
